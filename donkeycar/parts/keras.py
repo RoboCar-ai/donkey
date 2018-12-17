cf71e972436114d87167ff5dbd7926945400b02e
Thomas Milas <itpro1994@gmail.com>
2018-12-16 21:33:28 -0800

    removed normalization

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -351,7 +351,7 @@ def default_linear_deep(input_shape):
     img_in = Input(shape=input_shape, name='img_in')
     x = img_in
     x = Cropping2D(cropping=((50, 0), (0, 0)))(x)  # trim 10 pixels off top
-    x = Lambda(lambda x: x / 127.5 - 1.)(x)  # normalize and re-center
+    # x = Lambda(lambda x: x / 127.5 - 1.)(x)  # normalize and re-center
     x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(x)
     x = Dropout(drop)(x)
     x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu')(x)
